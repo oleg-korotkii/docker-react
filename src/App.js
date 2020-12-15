bb67ceb95fbaa04a8b9807f9db752612c2ecaa56
Oleg Korotky <korotky@gmail.com>
2020-12-15 18:24:02 +0700

    added feature branch

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Превед медвед!
+          feature branch
         </p>
         <a
           className="App-link"
